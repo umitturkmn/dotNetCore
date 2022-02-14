3d1dfd7c1be0bb3fc38641f7817a2b7d0a710f0a
umit482 <umit.482@gmail.com>
2022-02-14 15:10:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace dotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time <= 18)
+            {
+                Console.WriteLine("İyi ggünler!");
+            } else
+            {
+                Console.WriteLine("İyi Akşamlar...");
+            }
+
+
+        }
+    }
+}
+    
